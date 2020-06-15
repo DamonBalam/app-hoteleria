9c19672a96cb972df87a25c02330bcf91591c1de
Arturo Saldivar <damonbalam@gmail.com>
2020-06-15 11:32:57 -0500

    Fix Menú Lateral

--- src/components/SideComponent.vue
+++ src/components/SideComponent.vue
@@ -32,6 +32,33 @@
                     <v-list-item-title>{{ item.title }}</v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
+
+            <!-- vehicular -->
+            <v-list-group prepend-icon="mdi-cog" color="white" no-action>
+                <template v-slot:activator>
+                    <v-list-item-title>Configuración</v-list-item-title>
+                </template>
+
+                <!-- Catálogos -->
+                <v-list-group sub-group  color="white" no-action>
+                    <template v-slot:activator>
+                        <v-list-item-title>Catálogos</v-list-item-title>
+                    </template>
+
+                    <v-list-item
+                            color="white"
+                            v-for="(item, i) in catalogos"
+                            :key="i"
+                            link
+                            :to="item.path"
+                    >
+                        <v-list-item-title v-text="item.title"></v-list-item-title>
+                        <v-list-item-icon>
+                            <v-icon v-text="item.icon"></v-icon>
+                        </v-list-item-icon>
+                    </v-list-item>
+                </v-list-group>
+            </v-list-group>
         </v-list>
         <template v-slot:append>
             <div class="pa-2">
@@ -50,8 +77,13 @@
                     {title: "Huéspedes", icon: "mdi-account-supervisor", path: "/huesped-lista"},
                     {title: "Venta", icon: "mdi-cart", path: "/punto-venta"},
                     {title: "Caja", icon: "mdi-inbox-full", path: "/caja"},
-                    {title: "Reportes", icon: "mdi-file-chart", path: "/reportes"},
-                    {title: "Configuración", icon: "mdi-cogs", path: "/configuracion"}
+                    {title: "Reportes", icon: "mdi-file-chart", path: "/reportes"}
+                ],
+                catalogos:[
+                    {title: "Usuarios", icon: "mdi-account-network"},
+                    {title: "Habitaciones", icon: "mdi-bed"},
+                    {title: "Artículos", icon: "mdi-basket"},
+                    {title: "Servicios", icon: "mdi-room-service"},
                 ],
                 color: "primary",
                 colors: ["primary", "blue", "success", "red", "teal"],
